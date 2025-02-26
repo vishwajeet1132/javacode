cfa5933ac7d6a8b4adab720c9fb8d9e2706c4eab
vishwajeet1132 <30820588+vishwajeet1132@users.noreply.github.com>
2025-02-26 12:41:51 +0000

    variables file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "vishwajeet@gmail.com"
+var acoountPasword = "12345"
+accoutnCity = "Gurugram"
+
+// accountId = 2 // not allowed
+accountEmail = "hello@mn.com"
+acoountPasword = 212121
+accoutnCity = "Jaipur"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(acoountPasword);
+console.log(accoutnCity);
+console.table([accountId,accountEmail,acoountPasword,accoutnCity])
