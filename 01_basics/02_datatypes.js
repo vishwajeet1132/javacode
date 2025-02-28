6223f53a8312b647f641b505e0a8057de5db80cb
vishwajeet1132 <30820588+vishwajeet1132@users.noreply.github.com>
2025-02-28 10:26:15 +0000

    datatypes discussed

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,23 @@
 "use strict";
 //alert(3+3) // we are using nodeJS not browser
+
+
+let name = "Vishwajeet"
+let age = 18
+let isLoggedIn =false
+let state;
+
+// primitive datatypes
+// number =>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value means empty type is object
+//undefined => not defined yet type is undefined
+//symbol => unique 
+
+
+//object
+console.log(typeof null)
+
+
